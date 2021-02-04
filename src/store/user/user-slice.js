0cfeacb5b81a0e70ba0f9afd36ae96f853272be6
Danis <danis.haertdinov@gmail.com>
2021-02-05 01:04:02 +0400

    add user-slice and test it

---
+++ src/store/user/user-slice.js
@@ -0,0 +1,23 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const userSlice = createSlice({
+  name: `user`,
+  initialState: {
+    isAuthorized: false,
+    info: {
+      email: ``
+    }
+  },
+  reducers: {
+    authorize: ((state, action) => {
+      const email = action.payload;
+
+      state.info.email = email;
+      state.isAuthorized = true;
+    })
+  }
+});
+
+export const {authorize} = userSlice.actions;
+
+export default userSlice.reducer;
