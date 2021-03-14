7e5d583b2b889171ae4f115caeb98c1ae85dfcb7
Danis <danis.haertdinov@gmail.com>
2021-03-14 16:00:15 +0400

    move routes paths to const.ts

--- src/const/const.ts
+++ src/const/const.ts
@@ -1,3 +1,10 @@
 const OFFER_MAX_RATING = 5;
 
-export {OFFER_MAX_RATING};
+enum ROUTES {
+  MAIN = `/`,
+  OFFER =`/offer`,
+  LOGIN = `/login`,
+  FAVORITES = `/favorites`
+}
+
+export {OFFER_MAX_RATING, ROUTES};
